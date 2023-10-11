f2a11a950b9e70b1c17b88b54a238c7f654cce00
Luis-Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2023-10-11 14:17:32 -0300

    Update conectaApi.js

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,7 +1,7 @@
 async function listaProjetos(){
     const conexao = await fetch("https://imgport.netlify.app/info/db.json");
     const conexaoConvertida = await conexao.json();
-
+    console.log(conexao)
     return conexaoConvertida;
 }
 
